790600e46b29a464c36f1370e4c9a2b97c38aac2
SEUNGAH YOO <68517303+SeungAh-Yoo99@users.noreply.github.com>
2021-01-06 18:39:13 +0900

    02. MTV - 05. MTV의 T,V만들기 (1) - 회원가입

--- 03. Framework Django 기초/fc_community/fc_community/urls.py
+++ 03. Framework Django 기초/fc_community/fc_community/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # fcuser 아래로 오는 모든 url들은 fcuser.urls 안에 있는 url에서 관리
+    path('fcuser/', include('fcuser.urls'))
 ]
--- 03. Framework Django 기초/fc_community/fcuser/views.py
+++ 03. Framework Django 기초/fc_community/fcuser/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def register(request):  # url에 연결 시 요청 정보가  'request'라는 변수를 통해서 들어온다.
+    return render(request, 'register.html')
