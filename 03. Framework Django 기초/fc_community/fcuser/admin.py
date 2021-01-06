c3dc7ca62ca55aee85861d896236998ed7903cff
SEUNGAH YOO <68517303+SeungAh-Yoo99@users.noreply.github.com>
2021-01-06 17:38:39 +0900

    02. MTV - 04. Admin 활용

--- 03. Framework Django 기초/fc_community/fcuser/admin.py
+++ 03. Framework Django 기초/fc_community/fcuser/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Fcuser  # model 안에 있는 Fcuser 클래스를 가져온다.
 
 # Register your models here.
+
+
+class FcuserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'password')  # 어느 정보를 출력하고 싶은지 명시 할 수 있다.
+
+
+admin.site.register(Fcuser, FcuserAdmin)
--- 03. Framework Django 기초/fc_community/fcuser/models.py
+++ 03. Framework Django 기초/fc_community/fcuser/models.py
@@ -9,5 +9,10 @@ class Fcuser(models.Model):
     registered_dttm = models.DateTimeField(
         auto_now_add=True, verbose_name='등록시간')
 
+    def __str__(self):  # 클래스를 문자열로 변환 했을 때 어떻게 변환 할 지 정해주는 내장 함수
+        return self.username
+
     class Meta:
         db_table = 'fastcampus_fcuser'
+        verbose_name = '패스트캠퍼스 사용자'
+        verbose_name_plural = '패스트캠퍼스 사용자'  # 복수형 지정
