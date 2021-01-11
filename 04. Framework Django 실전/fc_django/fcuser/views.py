3a20d83d5ea493d274ee219db498601db715e011
SEUNGAH YOO <68517303+SeungAh-Yoo99@users.noreply.github.com>
2021-01-11 16:27:19 +0900

    03. View - 02. 회원 가입 만들기

--- 04. Framework Django 실전/fc_django/fcuser/views.py
+++ 04. Framework Django 실전/fc_django/fcuser/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from django.views.generic.edit import FormView
+from .forms import RegisterForm
 # Create your views here.
+
+
+def index(request):
+    return render(request, 'index.html')
+
+
+class RegisterView(FormView):
+    template_name = 'register.html'
+    form_class = RegisterForm
+    success_url = '/'  # 정상적으로 모든 정보가 입력 됐을 때 이동할 주소
