d2ba3d0a329c84599f6b58c3de23fc7a647527cd
SEUNGAH YOO <68517303+SeungAh-Yoo99@users.noreply.github.com>
2021-01-17 03:46:21 +0900

    04. 기본 스크랩핑 - 01. urllib 사용법 및 기초 스크랩핑 - 2

---
+++ 08. 크롤링 기초/python_crawl/section02-1.py
@@ -0,0 +1,9 @@
+# Section02-1
+# 파이썬 크롤링 기초
+# urllib 사용법 및 기본 스크랩핑
+
+import urllib.request as req
+
+# 파일 URL
+img_url = 'https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMDAzMjFfMTAz%2FMDAxNTg0NzY0ODM1ODk5.gx6e3nGwsp_Gas0_4Bu4LQPMj0uQw80BuZ46SUSOwN4g.6tmZ74la0Wrgf-Dm6US_wRvObmlQ7h2CWseG2bwIjhcg.JPEG.du_90%2FIMG_3095.JPG&type=sc960_832'
+html_url = ''
