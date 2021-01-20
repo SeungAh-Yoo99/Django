49d3d49578d48dc925bdfba6fe0440efc3e4fbba
SEUNGAH YOO <68517303+SeungAh-Yoo99@users.noreply.github.com>
2021-01-20 20:21:53 +0900

    10. 기본 스크랩핑 - 07. lxml 사용 기초 스크랩핑 B -1

---
+++ 08. 크롤링 기초/python_crawl/section02-4.py
@@ -0,0 +1,42 @@
+# Section02-4
+# 파이썬 크롤링 기초
+# lxml 사용 기초 스크랩핑(2)
+
+import requests
+from lxml.html import fromstring, tostring
+
+def main():
+    """
+    네이버 메인 뉴스 스탠드 스크랩핑 메인함수
+    """
+
+    # 세션 사용
+    session = requests.Session()
+
+    # 스크랩핑 대상 URL
+    response = session.get("https://www.naver.com") # Get, Post
+
+    # 신문사 링크 리스트 획득
+    urls = scrape_news_list_page(response)
+
+    # 결과 출력
+    for url in urls:
+        # url 출력
+        print (url)
+
+def scrape_news_list_page(response):
+    # URL 리스트 선언
+    urls = []
+
+    # 태그 정보 문자열 저장
+    root = fromstring(response.content)
+
+    for a in root.cssselect('.thumb_area .thumb_box .popup_wrap a[href^="http"]'):
+        # 링크
+        url = a.get('href')
+        urls.append(url)
+    return urls
+
+# 스크랩핑 시작
+if __name__ == "__main__":
+    main()
