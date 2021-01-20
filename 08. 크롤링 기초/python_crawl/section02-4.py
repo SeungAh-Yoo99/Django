fefaa5bbd1dbfd678a930bcb9bb05414225eed2e
SEUNGAH YOO <68517303+SeungAh-Yoo99@users.noreply.github.com>
2021-01-20 22:44:50 +0900

    11. 기본 스크랩핑 - 08. lxml 사용 기초 스크랩핑 B - 2

--- 08. 크롤링 기초/python_crawl/section02-4.py
+++ 08. 크롤링 기초/python_crawl/section02-4.py
@@ -16,26 +16,45 @@ def main():
     # 스크랩핑 대상 URL
     response = session.get("https://www.naver.com") # Get, Post
 
-    # 신문사 링크 리스트 획득
+    # 신문사 링크 딕셔너리 획득
     urls = scrape_news_list_page(response)
 
+    # # 딕셔너리 확인
+    # print(urls)
+
     # 결과 출력
-    for url in urls:
+    for name, url in urls.items():
         # url 출력
-        print (url)
+        print (name, url)
 
 def scrape_news_list_page(response):
-    # URL 리스트 선언
-    urls = []
+    # URL 딕셔너리 선언
+    urls = {}
 
     # 태그 정보 문자열 저장
     root = fromstring(response.content)
 
-    for a in root.cssselect('.thumb_area .thumb_box .popup_wrap a[href^="http"]'):
-        # 링크
-        url = a.get('href')
-        urls.append(url)
+    for a in root.xpath('//div[@class="thumb_area"]/div[@class="thumb_box _NM_NEWSSTAND_THUMB _NM_NEWSSTAND_THUMB_press_valid"]'):
+        # a 구조 확인
+        # print(a)
+
+        # a 문자열 출력
+        # print(tostring(a, pretty_print=True))
+
+        name, url = extract_contents(a)
+        # 딕셔너리 삽입
+        urls[name] = url
     return urls
+
+def extract_contents(dom):
+    # 링크 주소
+    link = dom.xpath('./div/a[@class="btn_popup"]')[0].get("href")
+
+    # 신문사 명
+    name = dom.xpath('./a[@class="thumb"]/img')[0].get("alt")
+    
+
+    return name, link
 
 # 스크랩핑 시작
 if __name__ == "__main__":
