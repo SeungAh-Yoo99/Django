205729cdcf902f11f0965196b6a4cff88fc977ec
SEUNGAH YOO <68517303+SeungAh-Yoo99@users.noreply.github.com>
2021-01-21 22:56:40 +0900

    18. Request - 01. request 사용 스크랩핑 A - 1

---
+++ 08. 크롤링 기초/python_crawl/section04-1.py
@@ -0,0 +1,20 @@
+# Section04-1
+# Requests
+# requests 사용 스크랩핑(1) - Session
+
+import requests
+
+# 세션 활성화
+# s = requests.Session()
+# r = s.get('http://www.naver.com')
+
+# 수신 데이터
+# print(r.text)
+
+# 수신 상태 코드
+# print('Status Code : {}'.format(r.status_code))
+# print('Ok? : {}'.format(r.ok))
+
+# 세션 비활성화
+# s.close()
+
