e1f9de36965eebba125a71c5d9cbe961a0748978
SEUNGAH YOO <68517303+SeungAh-Yoo99@users.noreply.github.com>
2021-01-21 16:32:58 +0900

    13. 기본 스크랩핑 실습 - 02. Get 방식 데이터 통신 - 2

--- 08. 크롤링 기초/python_crawl/section03-1.py
+++ 08. 크롤링 기초/python_crawl/section03-1.py
@@ -18,3 +18,26 @@ print('headers : {}'.format(mem.getheaders()))
 print('getcode : {}'.format(mem.getcode()))  # status와 같은 결과값 출력
 print('read : {}'.format(mem.read(100).decode('utf-8'))) # 100byte를 읽어온다
 print('parse : {}'.format(urlparse('http://www.encar.co.kr?test=test').query))
+
+# 기본 요청2(ipify)
+API = "https://api.ipify.org"
+
+# GET 방식 Parameter
+values = {
+    'format': 'json' # text, jsonp
+}
+
+print('before param : {}'.format(values))
+params = urllib.parse.urlencode(values)
+print('after param : {}'.format(params))
+
+# 요청 URL 생성
+URL = API + "?" + params
+print("요청 URL = {}".format(URL))
+
+# 수신 데이터 읽기
+data = urllib.request.urlopen(URL).read()
+
+# 수신 데이터 디코딩
+text = data.decode('UTF-8')
+print('response : {}'.format(text))
