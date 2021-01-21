cf8d1e132389a70a750997c749e60193aa3120aa
SEUNGAH YOO <68517303+SeungAh-Yoo99@users.noreply.github.com>
2021-01-22 01:19:43 +0900

    23. Reequest - 06. request 사용 스크랩핑 C - 2

--- 08. 크롤링 기초/python_crawl/section04-3.py
+++ 08. 크롤링 기초/python_crawl/section04-3.py
@@ -15,17 +15,74 @@ import requests
 # 세션 활성화
 s = requests.Session()
 
-# 예제1
+
+# 예제1(GET)
 r = s.get('https://api.github.com/events')
 
 # 수신상태 체크
 r.raise_for_status()
 
 # 출력
-print(r.text)
+# print(r.text)
 
-# 에제2
+
+# 에제2(GET)
 # 쿠키설정
 jar = requests.cookies.RequestsCookieJar()
 # 쿠키 삽입
 jar.set('name', 'niceman', domain="httpbin.org", path='/cookies')
+
+# 요청
+r = s.get('http://httpbin.org/cookies', cookies=jar)
+
+# 출력
+# print(r.text)
+
+
+# 예제3(GET)
+r = s.get('https://github.com', timeout=5)
+
+# 출력
+# print(r.text)
+
+
+# 예제4(POST)
+r = s.post('http://httpbin.org/post', data={'id':'test1', 'pw':'1111'}, cookies=jar)
+
+# 출력
+# print(r.text)
+# print(r.headers)
+
+
+#예제5(POST)
+payload1 = {'id':'test1', 'pw': '1111'}
+payload2 = (('id', 'test2'), ('pw', '2222'))
+
+r1 = s.post('http://httpbin.org/post', data=payload1)
+r2 = s.post('http://httpbin.org/post', data=payload2)
+
+# 출력
+# print(r1.text)
+# print(r2.text)
+
+
+# 예제6(PUT)
+r = s.put('http://httpbin.org/put', data=payload1)
+
+# 출력
+# print(r.text)
+
+
+# 예제7(DELETE)
+r = s.delete('http://httpbin.org/delete', data={'id' : 1}) # id가 1인 data를 삭제하길 원함
+
+# 출력
+# print(r.text)
+
+
+# 예제8(DELETE)
+r = s.delete('https://jsonplaceholder.typicode.com/posts/1')
+print(r.ok)
+print(r.text)
+
+s.close()
