d246633f814e3305a6d249c554a24c35f897084a
SEUNGAH YOO <68517303+SeungAh-Yoo99@users.noreply.github.com>
2021-02-02 14:51:28 +0900

    06. 6회차 - 커스텀필드 포매팅

--- 05. Framework Django 프로젝트/fc_django/product/admin.py
+++ 05. Framework Django 프로젝트/fc_django/product/admin.py
@@ -1,10 +1,23 @@
 from django.contrib import admin
+from django.utils.html import format_html
+from django.contrib.humanize.templatetags.humanize import intcomma # setting app에 humanize가 추가되어 있어야 한다.
 from .models import Product
 # Register your models here.
 
 
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('name', 'price')
+    list_display = ('name', 'price_format', 'styled_stock')
 
+    def price_format(self, obj):
+        price = intcomma(obj.price)
+        return '{} 원'.format(price)
+
+    def styled_stock(self, obj):
+        if obj.stock <= 50:
+            return format_html('<b><span style="color:red">{} 개</span></b>'.format(intcomma(obj.stock)))
+        return '{} 개'.format(intcomma(obj.stock))
+
+    price_format.short_description = '가격'    
+    styled_stock.short_description = '재고'
 
 admin.site.register(Product, ProductAdmin)
